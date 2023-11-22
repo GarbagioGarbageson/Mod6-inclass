6c15f5aa6d287c197cb1366dbc52980f8340913a
GarbagioGarbageson <146142358+GarbagioGarbageson@users.noreply.github.com>
2023-11-21 19:16:18 -0500

    Update to Module 6 lab Class 2S

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,12 +1,3 @@
 /*
     Java Tutorial
 */
-
-let textvalue = "Hello World";
-const headingElement = document.querySelector("h1"):
-headingElement.textContent = textvalue;
-
-const imageElement = document.querySelector("img");
-imageElement.style.display = "none";
-
-imageElement.classList.add('gone')
